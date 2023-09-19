92297661c8b006506536981387037e67b2243765
harsh.bhabarr@gmail.com <harsh.bhabarr@gmail.com>
2023-09-19 21:35:49 +0530

    search a 2d atrix

---
+++ Binary Search/Search in a 2D matrix.cpp
@@ -0,0 +1,29 @@
+class Solution
+{
+public:
+	bool searchMatrix(vector<vector<int>> &mat, int target)
+	{
+		int m = mat.size();
+		int n = mat[0].size();
+		int i = 0;
+		int j = n - 1;
+
+		while (i < m and j >= 0)
+		{
+			if (mat[i][j] == target)
+			{
+				return true;
+			}
+			else if (mat[i][j] < target)
+			{
+				i++;
+			}
+			else
+			{
+				j--;
+			}
+		}
+
+		return false;
+	}
+};
